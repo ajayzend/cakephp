87f5a0bd0b1ac38e9875721f6787a91f30949c67
Ajay <ajay.kumar.iimt@gmail.com>
2018-02-04 11:32:00 +0530

    Added some columns in table

--- dbchanges/29Jan2018.sql
+++ dbchanges/29Jan2018.sql
@@ -1,5 +1,8 @@
 ALTER TABLE `cars` ADD COLUMN `created_by` INT(10) NULL AFTER `isrecent`;
 
+ALTER TABLE cars ADD COLUMN `modified_by` INT(10) NULL AFTER `created_by`;
+
 update cars set created_by = 1;
+update cars set modified_by = 1;
 
 update car_payments set created_by = 1;
